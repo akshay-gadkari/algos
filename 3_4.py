fdc8e41448c851a94f126e6c93ac102e71323ac1
Akshay Gadkari <akshay.gadkari@gmail.com>
2019-04-30 16:35:09 -0700

    3.4

---
+++ 3_4.py
@@ -0,0 +1,27 @@
+# Implement a queue using two stacks.
+
+s1 = [1, 2, 3]
+s2 = []
+
+def isEmpty(s1, s2):
+    if len(s1) == 0 and len(s2) == 0:
+        return True
+    else:
+        return False
+
+def size(s1, s2):
+    size = len(s1) + len(s2)
+    return size
+print(size(s1, s2))
+
+def enqueue(e):
+    s1.append(e)
+
+def dequeue():
+    if len(s2) == 0:
+        while len(s1 != 0):
+            s2.append(s1[(len(s1)) - 1])
+    return s2.pop
+
+# enqueue(4)
+print(s1)
--- algos.py
+++ algos.py
@@ -1,5 +1,15 @@
-#open a file
-
+# open a file
 file = open("test.txt", "r")
 print(file.readlines())
 file.close()
+
+# Upside-Down Pyramid Addition is the process of taking a list of numbers and consecutively adding them together until you reach one number.
+
+# When given the numbers 2, 1, 1 the following process occurs:
+
+#  2   1   1
+#    3   2 
+#      5
+
+def reverse(list):
+
