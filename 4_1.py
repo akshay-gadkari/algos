3cf4c1330e202e2221645577fc9fb475d8690848
Akshay Gadkari <akshay.gadkari@gmail.com>
2019-05-05 15:26:12 -0700

    added graph

--- 4_1.py
+++ 4_1.py
@@ -1,2 +1,9 @@
 """ Given a directed graph, design an algorithm to find out whether there is a route between two nodes. """
 
+graph = {'A': ['B', 'C'],
+         'B': ['C', 'D'],
+         'C': ['D'],
+         'D': ['C'],
+         'E': ['F'],
+         'F': ['C']}
+
