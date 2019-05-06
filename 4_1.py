d5c106ce0b09cb8adba30c0a7472571d659b11a1
Akshay Gadkari <akshay.gadkari@gmail.com>
2019-05-06 13:24:33 -0700

    iterate over keys

--- 4_1.py
+++ 4_1.py
@@ -1,5 +1,6 @@
 """ Given a directed graph, design an algorithm to find out whether there is a route between two nodes. """
 
+# graph dict from online
 graph = {'A': ['B', 'C'],
          'B': ['C', 'D'],
          'C': ['D'],
@@ -7,3 +8,14 @@ graph = {'A': ['B', 'C'],
          'E': ['F'],
          'F': ['C']}
 
+def find(g, a, b):
+    # look at a, if the values of key a include b, return yes
+    for key, value in graph.items():
+        if key == a:
+            for i in value:
+                if i == b:
+                    print('true')
+                    return
+            # if not, search the values of those keys
+            
+find(graph, 'A', 'B')
