6a8172dc6ab860f11dc46501ba3ebc27d6b029fe
Akshay Gadkari <akshay.gadkari@gmail.com>
2019-05-13 14:37:13 -0700

    checking if project depends on another project

--- 4_7.py
+++ 4_7.py
@@ -6,6 +6,23 @@
 # be built before the project is. Find a build order that will allow the projects to be built.
 # If there is no valid build order, return an error.
 
-projects = [p1, p2, p3, p4]
-dependencies = [(p1, p2), (p3, p4)]
+projects = ['p1', 'p2', 'p3', 'p4']
+dependencies = [['p1', 'p2'], ['p3', 'p4']]
+dependencies1 = []
 
+def build(p, d):
+    # for i in range(len(dependencies)):
+    #     if dependencies[i][0] not in dependencies[i][1]:
+    #         print(dependencies[i][0])
+    #         print(dependencies[i][1])
+    #     else:        
+    #         return('not good enough')
+    for i in range(len(dependencies)):
+        dependencies1.append(dependencies[i][1])
+        print(dependencies1)
+        if dependencies[i][0] in dependencies1:
+            for i in dependencies1:
+                
+    return
+
+build(projects, dependencies)
