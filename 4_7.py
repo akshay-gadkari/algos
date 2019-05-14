ae2b940f1b8674dd2c33b94c3dcc1193371f4d83
Akshay Gadkari <akshay.gadkari@gmail.com>
2019-05-14 12:47:59 -0700

    added counter

--- 4_7.py
+++ 4_7.py
@@ -11,18 +11,24 @@ dependencies = [['p1', 'p2'], ['p3', 'p4']]
 dependencies1 = []
 
 def build(p, d):
-    # for i in range(len(dependencies)):
-    #     if dependencies[i][0] not in dependencies[i][1]:
-    #         print(dependencies[i][0])
-    #         print(dependencies[i][1])
-    #     else:        
-    #         return('not good enough')
     for i in range(len(dependencies)):
+        # list all the dependent projects
         dependencies1.append(dependencies[i][1])
-        print(dependencies1)
+        # print(dependencies1)
+        # if something is depended on, but also depends on something else
         if dependencies[i][0] in dependencies1:
-            for i in dependencies1:
-                
+            # check what it depends on, and what that depends on, and ...
+            check(dependencies[i][0])
     return
 
 build(projects, dependencies)
+
+counter = 0
+def check(dependency, counter):
+    for i in range(len(dependencies)):
+        if dependency == dependencies[i][1]:
+            dependency == dependencies[i][0]
+        else:
+            first_dependencies.append(dependency)
+            print(first_dependencies)
+    return
