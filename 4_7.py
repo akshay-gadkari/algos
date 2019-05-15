60d49e8f39a66f27d6a19bf679242855028eaa0c
Akshay Gadkari <akshay.gadkari@gmail.com>
2019-05-14 19:43:19 -0700

    added break

--- 4_7.py
+++ 4_7.py
@@ -7,32 +7,38 @@
 # If there is no valid build order, return an error.
 
 projects = ['p1', 'p2', 'p3', 'p4']
-dependencies = [['p1', 'p2'], ['p3', 'p4']]
+dependencies = [['p1', 'p2'], ['p3', 'p4'], ['p0', 'p1']]
 dependencies1 = []
-
-def build(p, d):
-    for i in range(len(dependencies)):
-        # list all the dependent projects
-        dependencies1.append(dependencies[i][1])
-        # print(dependencies1)
-        # if something is depended on, but also depends on something else
-        if dependencies[i][0] in dependencies1:
-            # check what it depends on, and what that depends on, and ...
-            print('true')
-            check(dependencies[i][0])
-            return
-    print('false')
-    return
-
-build(projects, dependencies)
+first_dependencies = []
 
 counter = 0
 def check(dep):
     for i in range(len(dependencies)):
         if dep == dependencies[i][1]:
             dep == dependencies[i][0]
-            print(dep)
+            # print(dep)
         else:
-            first_dependencies.append(dep)
-            print(first_dependencies)
+            if dep in first_dependencies:
+                break
+            else:
+                first_dependencies.append(dep)
+    print('first', first_dependencies)
     return
+
+def build(p, d):
+    for i in range(len(dependencies)):
+        # put all the dependent projects in a list
+        dependencies1.append(dependencies[i][1])
+        # if something is depended on, but also depends on something else
+        dependencies[i][0]
+        # print(dependencies[i][0])
+    for i in range(len(dependencies)):
+        if (dependencies[i][0] in dependencies1):
+            # check what it depends on, and what that depends on, and ...
+            # print('true')
+            check(dependencies[i][0])
+    # print('false')
+    # print(dependencies1)
+    return
+
+build(projects, dependencies)
