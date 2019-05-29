4f7bb6455a9ee0a186cf1f204efa5ec3d2afc901
Akshay Gadkari <akshay.gadkari@gmail.com>
2019-05-28 19:29:49 -0700

    added node

--- 4_10.py
+++ 4_10.py
@@ -5,3 +5,19 @@
 # if there exists a node n in T1 such that the subtree of n is identical to T2. That is,
 # if you cut off the tree at node n, the two trees would be identical.
 
+class node:
+    def __init__(self, v, left=None, right=None):
+        self.v = v
+        self.left = left
+        self.right = right
+
+node1 = node(1)
+node2 = node(2)
+node3 = node(3)
+node4 = node(4)
+node5 = node(5)
+
+node1.left = node2
+node1.right = node3
+node2.left = node4
+node2.right = node5
