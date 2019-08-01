aac87a50f57aceb05ff2ac47ef56bb38f652f301
Akshay Gadkari <akshay.gadkari@gmail.com>
2019-08-01 11:40:18 -0700

    some of 1.8

---
+++ 1_8.py
@@ -0,0 +1,23 @@
+""" 1.8 Zero Matrix: Write an algorithm such that if an element in an MxN matrix is 0, its entire row and column are set to 0. """
+
+m = [[1,  2,  3,  4 ],
+     [5,  6,  0,  8 ],
+     [9,  10, 11, 12 ]]
+
+def zero_matrix(m):
+    m2 = []
+    list_of_zeroes = []
+    zeroes = 0
+    for i in range(len(m)):
+        print('i', i)
+        for j in range(len(m[0])):
+            print('j is', j)
+            if m[i][j] == 0:
+                print(m[i][j], i, j)
+                m2.append(i)
+                m2.append(j)
+                list_of_zeroes.append(m2)
+                m2 = []
+    print(list_of_zeroes)
+
+zero_matrix(m)
