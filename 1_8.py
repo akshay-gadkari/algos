65d3f9436ea2249c6addfb2b5302fce1e1d3aa6e
Akshay Gadkari <akshay.gadkari@gmail.com>
2019-08-05 08:09:39 -0700

    1.8

--- 1_8.py
+++ 1_8.py
@@ -4,35 +4,28 @@ m = [[1,  2,  3,  4 ],
      [5,  6,  0,  8 ],
      [9,  10, 11, 12 ]]
 
-# def zero_matrix(m):
-#     m2 = []
-#     list_of_zeroes = []
-#     zeroes = 0
-#     rowlen = len(m[0])
-#     for i in range(len(m)):
-#         # print('i', i)
-#         for j in range(len(m[0])):
-#             # print('j is', j)
-#             if m[i][j] == 0:
-#                 print(m[i][j], i, j)
-#                 m2.append(i)
-#                 m2.append(j)
-#                 list_of_zeroes.append(m2)
-#                 print('m[i]', m[i])
-#                 m2 = []
-#     print(m)
-#     row = list_of_zeroes[0][0]
-#         m[row] = []
-#     print(list_of_zeroes[0][0])
+def zero_matrix(m):
+    m2 = []
+    list_of_zeroes = []
+    zeroes = 0
+    rowlen = len(m[0])
+    for i in range(len(m)):
+        # print('i', i)
+        for j in range(len(m[0])):
+            # print('j is', j)
+            if m[i][j] == 0:
+                print(m[i][j], i, j)
+                m2.append(i)
+                m2.append(j)
+                list_of_zeroes.append(m2)
+                print('m[i]', m[i])
+                # m2 = []
+    print(m2)
 
 # def zero_matrix(m):
 #     for i in range(len(m)):
 #         if 0 in m[i]:
 #             print(0, "in row", i)
 #     return
-
-def locate_zero_rows(matrix: list) -> list:
-    """Given an NxM matrix find the rows that contain a zero."""
-    return [i for i, row in enumerate(matrix) if not all(row)]
 
 zero_matrix(m)
