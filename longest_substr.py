14c3688c2b93b9124ec705f809eabd3101bc24fd
Akshay Gadkari <akshay.gadkari@gmail.com>
2019-08-07 14:04:15 -0700

    longest substring partial solution

--- longest_substr.py
+++ longest_substr.py
@@ -19,3 +19,23 @@
 # Explanation: The answer is "wke", with the length of 3. 
 #              Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
 
+s = 'abcdefgggghijklmnopqrstuvwxyz'
+def longest_substr(s):
+    s2 = ''
+    longest = ''
+    for i in range(len(s)):
+        if s[i] != s[i-1]:
+            s2 = s2 + s[i]
+            # print('same')
+            #print(s2)
+        else:
+            if len(s2) < len(longest):
+                longest += s2
+                s2 = ''
+    if len(s2) < len(longest):
+        print(s2)
+    else:
+        print(longest)
+    return
+
+longest_substr(s)
