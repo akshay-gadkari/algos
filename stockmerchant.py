f6697a1dd782eb2b02aae12e90f263a9d9898e96
Akshay Gadkari <akshay.gadkari@gmail.com>
2019-08-16 21:22:51 -0700

    sock merchant

---
+++ stockmerchant.py
@@ -0,0 +1,13 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+n = 7
+ar = [1,2,1,2,1,3,2]
+# Complete the sockMerchant function below.
+def sockMerchant(n, ar):
+    
